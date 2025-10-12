ef1a35d28036833a40f1437ea5e3cb0a33934a09
Shubham277353 <golubhandari405@gmail.com>
2025-10-12 12:39:58 +0530

    feat: add project sidebar and todo area in html

--- src/addTodos.js
+++ src/addTodos.js
@@ -1,9 +1,9 @@
 import createTodo from "./createTodos";
 
+const todoList = [];
 
 
 export function addTodos(){
-const todoList = [];
 
 const todo1 = new createTodo('Buy milk', 'From store', '2025-10-12','High','check the expiry date',false);
 todoList.push(todo1);
--- src/displayTodo.js
+++ src/displayTodo.js
@@ -1,3 +1,4 @@
+import "./style.css";
 import CreateTodo from "./createTodos";
 
 export default function display(){
