444b19c37e4f4dd5a8f6ad5a39b74846340c8b5f
Shubham277353 <golubhandari405@gmail.com>
2025-11-09 13:32:54 +0530

    feat : add new project pop up DoM logic

--- src/addNewProject.js
+++ src/addNewProject.js
@@ -14,14 +14,27 @@ export default function addNewProject() {
     projectTitle.classList.add("project-title-input");
     projectTitle.type = "text";
     projectTitle.placeholder = "Project name";
+    projectTitle.required = true;
 
     const projectCancelBtn = document.createElement("button");
-    projectCancelBtn.classList.add("project-cancel-btn");
+    projectCancelBtn.classList.add("project-cancel-btn","buttons");
     projectCancelBtn.textContent = "Cancel";
+    projectCancelBtn.addEventListener("click",()=>{
+      createProjectDiv.remove();
+    })
 
     const projectAddBtn = document.createElement("button");
-    projectAddBtn.classList.add("project-add-btn");
+    projectAddBtn.classList.add("project-add-btn","buttons");
+    projectAddBtn.type = "button"
     projectAddBtn.textContent = "Add";
+    projectAddBtn.addEventListener("click",()=>{
+      if (projectTitle.value.trim() === "") {
+        alert("Please enter a project name");
+        return;
+      }
+
+      export projectTitle.value ;
+    })
 
     createProjectDiv.append(projectHeadin,projectTitle,projectCancelBtn,projectAddBtn);
     projectList.append(createProjectDiv);
