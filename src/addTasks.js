435792ecf97f069f603cca1dcb9fa4ccb274413e
Shubham277353 <golubhandari405@gmail.com>
2026-01-14 21:23:59 +0530

    feat: adding ui to the app

--- src/addTasks.js
+++ src/addTasks.js
@@ -1,5 +1,7 @@
 import {projects} from './logic';
 import {activeProject} from './logic';
+import { addTask } from "./logic";
+
 
 const taskList = document.getElementById("task-list");
 
@@ -11,8 +13,12 @@ export default function renderTasks() {
 
     tasks.forEach(task =>{
         const newtask = document.createElement("li");
-        newtask.textContent = task.description;
+        newtask.textContent = task.dueDate;
         taskList.appendChild(newtask);
     });
 }
-
+const addTaskBtn = document.getElementById("add-task");
+addTaskBtn.addEventListener("click", ()=>{
+    addTask("Learn Arrays","understand the basics of it","10/11/2026","High");
+    renderTasks();
+})
--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
 import "./style.css";
-import { addTask } from "./logic";
 import renderTasks from "./addTasks";
 
-const addTaskBtn = document.getElementById("add-task");
-addTaskBtn.addEventListener("click", ()=>{
-    addTask("Learn Arrays","understand the basics of it","10/11/2026","High");
-    renderTasks();
-})
+renderTasks();
